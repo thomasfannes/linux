39efc171431b63eed2e64771635e704bce3ec1b2
Thomas Fannes <thomasfannes@gmail.com>
2020-01-21 08:01:23 +0100

    Added XFCE config

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,5 @@
+require 'fileutils'
+
 desc 'link the vimrc file'
 task :link do
     tgt = File.join(ENV['HOME'], ".vimrc")
@@ -5,4 +7,27 @@ task :link do
     if !File.exists?(tgt)
     	sh "ln -s #{src} #{tgt}"
     end
+end
+
+def link_it(src_dir, tgt_dir)
+  src_dir = File.absolute_path(src_dir)
+  tgt_dir = File.absolute_path(tgt_dir)
+  Dir.chdir(src_dir) do
+    Dir.glob("**/*", File::FNM_DOTMATCH).select {|f| File.file?(f) }.each do |f| 
+      dir = File.join(tgt_dir, File.dirname(f))
+
+      FileUtils.mkdir_p(dir) unless File.exist?(dir)
+
+      fn = File.join(dir, File.basename(f))
+      FileUtils.rm_f(fn) 
+      
+      FileUtils.ln_s(File.join(src_dir, f), dir)
+    end
+  end
+end
+
+
+desc 'install the xfce updates'
+task :xfce do
+  link_it("xfce", File.join(ENV['HOME'], "test"))
 end
